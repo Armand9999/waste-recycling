f621a731c053f1a5160197241834c2757d6c08d5
Armand9999 <armandnzeyang@yahoo.com>
2025-05-31 16:54:31 -0400

    Added icon to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "DHAFA Recycling",
   description: "Give Plastic a New Life.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
